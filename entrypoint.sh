079ce70129cbbbe6ec37f97884547aa33f06b4e4
Sean Lee <leejoneshane@gmail.com>
2017-11-23 13:04:52 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,8 @@ if [[ ! -f /etc/openldap/is.done ]]; then
     slapindex -f /etc/openldap/slapd.conf
     /etc/init.d/slapd restart
     smbpasswd -w $SAMBA_ADMIN_PASSWORD
+    smbldap-useradd -a admin
+    echo -e "$SAMBA_ADMIN_PASSWORD\n$SAMBA_ADMIN_PASSWORD" | smbpasswd -s admin
     touch /etc/openldap/is.done
 fi
 
