116250b0fdec6f28dd2155b9403eef4939d7e1d4
Sean Lee <leejoneshane@gmail.com>
2017-11-24 09:04:54 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 if [[ ! -f /etc/openldap/is.done ]]; then
     SECRET=`slappasswd -s "$SAMBA_ADMIN_PASSWORD" -n`
     sed -ri "s#SAMBA_ADMIN_SECRET#$SECRET#g" /etc/openldap/slapd.ldif
-    slapadd -v -l /etc/openldap/slapd.ldif
+#    slapadd -v -l /etc/openldap/slapd.ldif
     slapindex -f /etc/openldap/slapd.conf
     /etc/init.d/slapd restart
     smbpasswd -w $SAMBA_ADMIN_PASSWORD
