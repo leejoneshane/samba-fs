5640b2d46a9088867c88d5860fee0351129ac34d
Sean Lee <leejoneshane@gmail.com>
2017-11-24 12:17:17 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 set -e
 
-#if [[ ! -f /etc/openldap/is.done ]]; then
-#    SECRET=`slappasswd -s "$SAMBA_ADMIN_PASSWORD" -n`
-#    sed -ri "s#SAMBA_ADMIN_SECRET#$SECRET#g" /etc/openldap/slapd.ldif
-#    slapadd -v -l /etc/openldap/slapd.ldif
-#    slapindex -f /etc/openldap/slapd.conf
-#    slapd -f /etc/openldap/slapd.conf
-#    smbpasswd -w $SAMBA_ADMIN_PASSWORD
-#    echo -e "$SAMBA_ADMIN_PASSWORD\n$SAMBA_ADMIN_PASSWORD" | smbpasswd -s admin
-#    touch /etc/openldap/is.done
-#fi
+if [[ ! -f /etc/openldap/is.done ]]; then
+    SECRET=`slappasswd -s "$SAMBA_ADMIN_PASSWORD" -n`
+    sed -ri "s#SAMBA_ADMIN_SECRET#$SECRET#g" /etc/openldap/initldap.ldif
+    sed -ri "s#SAMBA_ADMIN_SECRET#$SECRET#g" /etc/openldap/slapd.conf
+    slapadd -v -l /etc/openldap/initldap.ldif
+    slapindex -f /etc/openldap/slapd.conf
+    slapd -f /etc/openldap/slapd.conf
+    smbpasswd -w $SAMBA_ADMIN_PASSWORD
+    echo -e "$SAMBA_ADMIN_PASSWORD\n$SAMBA_ADMIN_PASSWORD" | smbpasswd -as admin
+    touch /etc/openldap/is.done
+fi
 
 if [[ -f /etc/samba/smb.conf ]]; then
     exec smbd -FS
